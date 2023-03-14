cefac05bec5c23656d207497ec042128e19b0cf2
Tim James <121989305+timjames1999@users.noreply.github.com>
2023-03-13 22:23:36 -0400

    Plot1 Add
    Plot1 Add

--- plot1.R
+++ plot1.R
@@ -10,4 +10,12 @@ download.file(file_url, destfile = "./ExData_Plotting1/projectdataset.zip")
 unzip(zipfile = "./ExData_Plotting1/projectdataset.zip", exdir = "./ExData_Plotting1")
 
 #Read household power data
-hpower <- read.table("./ExData_Plotting1/household_power_consumption.txt")
+hpower <- read.table("./ExData_Plotting1/household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", na.strings = "?")
+
+#Subset the data for dates needed
+subsetdata <- hpower[hpower$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot Global Active Power in .png
+png("plot1.png", width=480, height=480)
+hist(subsetdata[, 3], col= "red",  main = "Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
